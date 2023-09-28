11947108f97fa2b7585658563ea8c26db3f968a6
amirsho0017 <amirshorakhimov0017>
2023-09-28 11:06:24 +0500

    day:11 nested loops

--- YandexHandBook/python/nestedloopTasks.py
+++ YandexHandBook/python/nestedloopTasks.py
@@ -1,4 +1,28 @@
 
+# E
+quan = int(input())
+q = 0
+k = 0
+while k < quan:
+    le = []
+    while (st := input()) != "ВСЁ":
+        le.append(st)
+    k += 1
+    if "зайка" in le:
+        q += 1
+print(q)
+
+# D
+quan = int(input())
+s = 0
+k = 0
+while k < quan:
+    st = str(input())
+    for i in st:
+        s += int(i)
+    k += 1
+print(s)
+
 # B
 num = int(input())
 for i in range(1, num + 1):

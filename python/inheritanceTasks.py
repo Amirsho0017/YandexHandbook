81659b2247f73998595b4b7e229a6839810e64dd
amirsho0017 <amirshorakhimov0017>
2023-12-14 09:14:59 +0500

    inheritanceTask

---
+++ python/inheritanceTasks.py
@@ -0,0 +1,26 @@
+class Fraction:
+    def __init__(self, numerator, denominator):
+        self.a = numerator
+        self.b = denominator
+
+    def numerator(self, number=None):
+        if number:
+            self.a = number
+        return self.a
+
+    def denominator(self, number=None):
+        if number:
+            self.b = number
+        return self.b
+
+    def __str__(self):
+        return f"{self.a}/{self.b}"
+
+    def __repr__(self):
+        return f"Fraction({self.a}, {self.b})"
+
+
+fraction = Fraction(3, 9)
+print(fraction, repr(fraction))
+print(fraction.denominator())
+print(fraction.numerator())

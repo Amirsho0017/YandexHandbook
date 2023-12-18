24a030d3db428abe909ab4c825301365db7aa43c
amirjonr <amirshorakhimov0017@gmail.com>
2023-12-18 15:35:51 +0500

    some task from request modeules

--- python/requestsModuleTasks.py
+++ python/requestsModuleTasks.py
@@ -1,15 +1,19 @@
-import requests
+from requests import get
+
+server_addr = input()
+key = input()
+
+response = get(f"http://{server_addr}")
+data = eval(response.content.decode('UTF-8'))
+
+if data.get(key):
+    print(data[key])
+else:
+    print('No data')
 
 server_address = input()
 
-total_sum = 0
-
-response = requests.get(f"http://{server_address}")
-while True:
-    data = int(response.content.decode('utf-8'))
-    if data == 0:
-        break
-    else:
-        total_sum += data
-
-print(total_sum)
+response = get(f"http://{server_address}")
+data = response.content.decode('utf-8')
+li = [i for i in data if isinstance(i, int)]
+print(sum(li))
